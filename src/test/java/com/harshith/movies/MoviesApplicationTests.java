2f5ad76d7ea05f8da78dd1f80bb4cea8954513eb
Harshith Teja <harshithtejadontoju@gmail.com>
2024-06-10 13:28:33 +0530

    added docker file

---
+++ src/test/java/com/harshith/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshith.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
